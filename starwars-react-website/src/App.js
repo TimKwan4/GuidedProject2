8c108a4659c2b97a84160a63da8e1db1ae212eaa
Dhir Patel <ddpatel@travelers.com>
2023-07-05 16:24:51 -0400

    React app

---
+++ starwars-react-website/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Character from './components/Character';
+import Film from './components/Film';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='character/:id' element={<Character />} />
+        <Route path='films/:id' element={<Film />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
